bbf657464086f6a4bf230ad502fd8c23ca6837a1
shweta20-13 <127878513+shweta20-13@users.noreply.github.com>
2024-03-28 15:52:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Navigation from './navigation';
+import Hero from'./hero';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navigation/>
+      <Hero/>
+      
+    </div>
+  );
+}
+
+export default App;
