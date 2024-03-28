91422ef92cd55a5a138c8b79e8cefaf478d2b354
sharmaprinceji <princesh1411@gmail.comi>
2024-03-28 15:28:03 +0530

    basic1

---
+++ src/hero.js
@@ -0,0 +1,33 @@
+function Hero(){
+    return (
+        <main className="hero1 container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVES THE BEST</h1>
+                <p>YOUR FEET DESERVES THE BEST AND WERE HERE TO HELP YOU
+                    WITH OUT SHOES.YOUR FEET DESERVES THE BEST AND WERE 
+                    HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="secondary-btn">Category</button>
+
+                </div>
+                <div className="Shopping">
+                    <p>ALSO AVAILABLE ON</p>
+                    <div className="Brand-icons">
+                        <img src='/img/flipkart.jfif'></img>
+                        <img src='/img/amazon.png'></img>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div className="hero-img">
+                <img src='/img/shoes.jfif'></img>
+               
+            </div>
+        </main>
+    )
+
+}
+export default Hero;
